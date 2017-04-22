a2e862fab144029860dbce114a6ea2ce3e642868
thompsonate <nate@natethompson.io>
2017-04-22 14:11:16 -0600

    Initial Commit

---
+++ Siri-Query-mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Siri-Query-mac
+//
+//  Created by Nate Thompson on 4/22/17.
+//  Copyright © 2017 SiriQuery. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Siri-Query-mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Siri-Query-mac
+//
+//  Created by Nate Thompson on 4/22/17.
+//  Copyright © 2017 SiriQuery. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
