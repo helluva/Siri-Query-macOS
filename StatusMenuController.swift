08d09d11d940f6e1157deb7428faaccc86b3ad84
thompsonate <nate@natethompson.io>
2017-04-22 17:30:39 -0600

    added recording and auto-triggering Siri before playing

--- StatusMenuController.swift
+++ StatusMenuController.swift
@@ -13,6 +13,7 @@ import AVFoundation
 class StatusMenuController: NSObject {
     
     var player: AVPlayer!
+    var recorder: AVAudioRecorder!
     @IBOutlet weak var statusMenu: NSMenu!
     
     let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
@@ -27,7 +28,23 @@ class StatusMenuController: NSObject {
     @IBAction func playClicked(_ sender: NSMenuItem) {
         let url = URL(fileURLWithPath: "/Users/Nate/Desktop/siriTest.mp3")
         player = AVPlayer(url: url)
-        player.play()
+        
+        NSWorkspace.shared().launchApplication("/Applications/Siri.app")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
+            self.player.play()
+        })
+    }
+    
+    @IBAction func recordClicked(_ sender: NSMenuItem) {
+        let url = URL(fileURLWithPath: "/Users/Nate/Desktop/nort.mp4")
+        recorder = try? AVAudioRecorder(url: url, settings: [AVFormatIDKey : kAudioFormatMPEG4AAC, AVSampleRateKey : 44100])
+        recorder.prepareToRecord()
+        recorder.record()
+    }
+    
+    @IBAction func stopClicked(_ sender: NSMenuItem) {
+        recorder.stop()
     }
     
     @IBAction func quitClicked(sender: NSMenuItem) {
